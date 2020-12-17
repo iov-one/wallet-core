b0d1c5e8b330eeb9848d480b8545c8e39b45120b
Antoine Herzog <antoine@iov.one>
2020-12-17 14:50:52 +0100

    fix import

--- swift/Tests/Blockchains/IOVTests.swift
+++ swift/Tests/Blockchains/IOVTests.swift
@@ -3,8 +3,8 @@
 // This file is part of Trust. The full Trust copyright notice, including
 // terms governing use, modification, and redistribution, is contained in the
 // file LICENSE at the root of the source code distribution tree.
-import TrustWalletCore
 import XCTest
+import WalletCore
 
 class IOVTests: XCTestCase {
 
